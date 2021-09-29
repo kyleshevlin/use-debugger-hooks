415d90e270b621d44b515723b24165eb41b236f8
Kyle Shevlin <kyle.a.shevlin@gmail.com>
2021-09-29 08:49:17 -0700

    adds functionality to handle sets and maps

--- src/utils.ts
+++ src/utils.ts
@@ -1,8 +1,13 @@
-export type Change<T> = {
-  name?: string;
+type Values<T> = {
   previousValue: T | undefined;
   currentValue: T;
 };
+
+export type Change<T> =
+  | Values<T>
+  | (Values<T> & { name: string })
+  | (Values<T> & { index: number })
+  | { onlyInPrevious: Set<unknown>; onlyInCurrent: Set<unknown> };
 
 type Changes<T> = Array<Change<T>>;
 
@@ -10,36 +15,117 @@ export function getChanges<T>(
   previousValue: T | undefined,
   currentValue: T
 ): Changes<T> {
-  // Handle non-null objects
+  // Nulls
   if (
-    typeof previousValue === 'object' &&
-    previousValue !== null &&
-    typeof currentValue === 'object' &&
-    currentValue !== null
+    (previousValue === null || currentValue === null) &&
+    previousValue !== currentValue
   ) {
-    const result = [] as Changes<T>;
-    const currentEntries = Object.entries(currentValue) as Array<[string, T]>;
-
-    for (const [key, value] of currentEntries) {
-      // @ts-ignore TODO: thinks `previousValue` is unknown and not an object
-      const oldValue: T = previousValue[key];
-
-      if (value !== oldValue) {
-        result.push({
-          name: key,
-          previousValue: oldValue,
-          currentValue: value,
-        });
-      }
-    }
-
-    return result;
+    return [{ previousValue, currentValue }];
   }
 
-  // Handle primitive values
+  // Arrays
+  if (Array.isArray(previousValue) && Array.isArray(currentValue)) {
+    return handleArrays<T>(previousValue, currentValue);
+  }
+
+  // Sets
+  if (previousValue instanceof Set && currentValue instanceof Set) {
+    return handleSets(previousValue, currentValue);
+  }
+
+  // Maps
+  if (previousValue instanceof Map && currentValue instanceof Map) {
+    return handleMaps(previousValue, currentValue);
+  }
+
+  // Objects
+  if (typeof previousValue === 'object' && typeof currentValue === 'object') {
+    return handleObjects<T>(previousValue, currentValue);
+  }
+
+  // Primitives
   if (previousValue !== currentValue) {
     return [{ previousValue, currentValue }];
   }
 
   return [];
+}
+
+function handleArrays<T>(previous: T, current: T) {
+  if (!Array.isArray(previous) || !Array.isArray(current)) {
+    throw new Error('Values are not arrays');
+  }
+
+  const result = [] as Changes<T>;
+
+  current.forEach((value, index) => {
+    const oldValue = previous[index];
+    if (value !== oldValue) {
+      result.push({
+        index,
+        previousValue: oldValue,
+        currentValue: value,
+      });
+    }
+  });
+
+  return result;
+}
+
+function handleObjects<T extends { [key: string]: any }>(
+  previous: T,
+  current: T
+) {
+  const result = [] as Changes<T>;
+  const entries = Object.entries(current) as [string, any][];
+
+  for (const [key, value] of entries) {
+    const oldValue: T = previous[key];
+
+    if (value !== oldValue) {
+      result.push({
+        name: key,
+        previousValue: oldValue,
+        currentValue: value,
+      });
+    }
+  }
+
+  return result;
+}
+
+function getDifference<T>(setA: Set<T>, setB: Set<T>) {
+  const result = new Set();
+
+  for (let element of setA) {
+    if (!setB.has(element)) {
+      result.add(element);
+    }
+  }
+
+  return result;
+}
+
+function handleSets<T extends Set<any>>(previous: T, current: T) {
+  const onlyInPrevious = getDifference<T>(previous, current);
+  const onlyInCurrent = getDifference<T>(current, previous);
+  return [{ onlyInPrevious, onlyInCurrent }];
+}
+
+function handleMaps<T extends Map<any, any>>(previous: T, current: T) {
+  const result = [] as Changes<T>;
+
+  for (const [key, value] of current.entries()) {
+    const oldValue = previous.get(key);
+
+    if (value !== oldValue) {
+      result.push({
+        name: key,
+        previousValue: oldValue,
+        currentValue: value,
+      });
+    }
+  }
+
+  return result;
 }
--- test/utils.test.ts
+++ test/utils.test.ts
@@ -1,20 +1,68 @@
 import { getChanges } from '../src/utils';
 
-test('getChanges', () => {
-  expect(getChanges(0, 0)).toEqual([]);
-  expect(getChanges(0, 1)).toEqual([{ previousValue: 0, currentValue: 1 }]);
-  expect(getChanges({ foo: 'foo' }, { foo: 'bar' })).toEqual([
-    {
-      name: 'foo',
-      previousValue: 'foo',
-      currentValue: 'bar',
-    },
-  ]);
-  expect(getChanges([0], [1])).toEqual([
-    {
-      name: '0',
-      previousValue: 0,
-      currentValue: 1,
-    },
-  ]);
+describe('getChanges', () => {
+  it('should return an empty array when there are no changes', () => {
+    expect(getChanges(0, 0)).toEqual([]);
+  });
+
+  it('should return an object of the different values for primitives', () => {
+    expect(getChanges(0, 1)).toEqual([{ previousValue: 0, currentValue: 1 }]);
+  });
+
+  it('should return an object of the different values if one is null', () => {
+    expect(getChanges([1], null)).toEqual([
+      {
+        previousValue: [1],
+        currentValue: null,
+      },
+    ]);
+  });
+
+  it('should indicate the key for differences in object values', () => {
+    expect(getChanges({ foo: 'foo' }, { foo: 'bar' })).toEqual([
+      {
+        name: 'foo',
+        previousValue: 'foo',
+        currentValue: 'bar',
+      },
+    ]);
+  });
+
+  it('should indicate the index for differences in array values', () => {
+    expect(getChanges([0], [1])).toEqual([
+      {
+        index: 0,
+        previousValue: 0,
+        currentValue: 1,
+      },
+    ]);
+  });
+
+  it('should handle Sets', () => {
+    const set1 = new Set([1, 2, 3]);
+    const set2 = new Set([2, 3, 4]);
+
+    expect(getChanges(set1, set2)).toEqual([
+      {
+        onlyInPrevious: new Set([1]),
+        onlyInCurrent: new Set([4]),
+      },
+    ]);
+  });
+
+  it('should handle Maps', () => {
+    const map1 = new Map();
+    const map2 = new Map();
+
+    map1.set('foo', 'foo');
+    map2.set('foo', 'bar');
+
+    expect(getChanges(map1, map2)).toEqual([
+      {
+        name: 'foo',
+        previousValue: 'foo',
+        currentValue: 'bar',
+      },
+    ]);
+  });
 });
